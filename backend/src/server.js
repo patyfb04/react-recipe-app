f43f5264159444ca7874af19890b2a04a3fdcb51
Patricia <patyfb04@gmail.com>
2026-02-16 21:15:22 -0500

    add job

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,12 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { eq, and } from "drizzle-orm";
+import job from "./config/cron.js";
 
 const PORT = ENV.PORT || 5001;
+
+if (process.env.NODE_ENV === "production") job.start();
+
 const app = express();
 
 app.use(express.json());
